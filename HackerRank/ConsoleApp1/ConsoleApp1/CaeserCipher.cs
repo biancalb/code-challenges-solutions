32533bbd0338fbccee93079b3a1ee61cc8baaa59
Bianca <biancalbarbosa@hotmail.com>
2024-01-11 09:09:28 -0300

    exercices solutions from hacker rank day3 preparation

---
+++ HackerRank/ConsoleApp1/ConsoleApp1/CaeserCipher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class CaeserCipher
+    {
+        public static void Encrypt(string s, int k)
+        {
+            var alphabet = Enumerable.Range('A', 26).Select(x => (char)x).ToList();
+            var encrypted = new List<char>();
+
+            foreach(var c in s)
+            {
+                if (!char.IsLetter(c))
+                {
+                    encrypted.Add(c);
+                }
+                else
+                {
+                    var isUpper = char.IsUpper(c);
+                    var index = alphabet.IndexOf(isUpper ? c : char.ToUpperInvariant(c));
+
+                    var rotationFactor = index + k;
+                    int encryptedIndex = rotationFactor;
+
+                    if(rotationFactor >= alphabet.Count)
+                    {
+                        encryptedIndex = rotationFactor % (alphabet.Count);
+                    }
+
+                    var newC = alphabet.ElementAt(encryptedIndex);
+                    encrypted.Add(isUpper ? newC : char.ToLowerInvariant(newC));
+                }
+            }
+            Console.WriteLine(string.Concat(encrypted));
+            //var str = "cdefghijklmnopqrstuvwxyzab".Select(x => x).ToArray();            
+            //var str = "1Y7U4WsDt23l4ww08E6zR3T19H4sWQ188N9bivyC6k1uNHAt1n10fz7fVk62XW2fyMU4D83am7R80N".Select(x => x).ToArray();
+            //var str = "Lipps_Asvph!".Select(x => x).ToArray();
+            //Console.WriteLine(string.Join(' ', str));
+        }
+    }
+}
---
+++ HackerRank/ConsoleApp1/ConsoleApp1/PalindromeIndex.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class PalindromeIndex
+    {
+        public static void Find(string s)
+        {
+            if(string.Concat(s.Reverse()) == s)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                var startIndex = 0;
+                var endIndex = s.Length - 1;
+                var indexesToRemove = new List<int>();
+
+                for (int i = 0; i < (s.Length / 2); i++)
+                {
+                    Console.WriteLine("startIndex: " + startIndex);
+                    Console.WriteLine("endIndex: " + endIndex);
+
+                    // to do: melhorar verificação
+                    if (s[startIndex] != s[endIndex])
+                    {
+                        Console.WriteLine("Found Diff");
+                        if (s[endIndex - 1] == s[startIndex])
+                        {
+
+                            indexesToRemove.Add(endIndex);
+                            startIndex++;
+                            endIndex -= 2;                            
+                        }
+                        else
+                        {
+                            indexesToRemove.Add(startIndex);
+                            startIndex += 2;
+                            endIndex --;
+                        }
+                    }
+                    else
+                    {
+                        startIndex++;
+                        endIndex--;
+                    }
+                    Console.WriteLine('\n');
+                }
+                Console.WriteLine(string.Join(' ', indexesToRemove));
+            }
+        }
+    }
+}
+
+
+//10
+//quyjjdcgsvvsgcdjjyq
+//hgygsvlfwcwnswtuhmyaljkqlqjjqlqkjlaymhutwsnwcflvsgygh
+//fgnfnidynhxebxxxfmxixhsruldhsaobhlcggchboashdlurshxixmfxxxbexhnydinfngf
+//bsyhvwfuesumsehmytqioswvpcbxyolapfywdxeacyuruybhbwxjmrrmjxwbhbyuruycaexdwyfpaloyxbcpwsoiqtymhesmuseufwvhysb
+//fvyqxqxynewuebtcuqdwyetyqqisappmunmnldmkttkmdlnmnumppasiqyteywdquctbeuwenyxqxqyvf
+//mmbiefhflbeckaecprwfgmqlydfroxrblulpasumubqhhbvlqpixvvxipqlvbhqbumusaplulbrxorfdylqmgfwrpceakceblfhfeibmm
+//tpqknkmbgasitnwqrqasvolmevkasccsakvemlosaqrqwntisagbmknkqpt
+//lhrxvssvxrhl
+//prcoitfiptvcxrvoalqmfpnqyhrubxspplrftomfehbbhefmotfrlppsxburhyqnpfmqlaorxcvtpiftiocrp
+//kjowoemiduaaxasnqghxbxkiccikxbxhgqnsaxaaudimeowojk
+
+//1
+//8
+//33
+//23
+//25
+//44
+//20
+//- 1
+//14
+//- 1
