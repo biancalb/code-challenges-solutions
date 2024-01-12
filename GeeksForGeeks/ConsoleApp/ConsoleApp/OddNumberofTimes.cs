07a9e9ad9d5cbc1ee3a22f063e732fc69afe24df
Bianca <biancalbarbosa@hotmail.com>
2024-01-12 14:06:51 -0300

    solution for a geeks4geeks problem

---
+++ GeeksForGeeks/ConsoleApp/ConsoleApp/OddNumberofTimes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class OddNumberofTimes
+    {
+        public static void FindNumber(int[] arr)
+        {
+            Console.WriteLine("arr= " + string.Join(' ', arr));
+            var ocurrsOddTimes = arr.GroupBy(x => x).Where(g => g.Count() % 2 != 0).Select(c => c.Key).FirstOrDefault();
+            Console.WriteLine(ocurrsOddTimes);
+        }
+    }
+}
---
+++ GeeksForGeeks/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the Number Occurring Odd Number of Times");
+            //var arr = new int[] { 1, 2, 3, 2, 3, 1, 3};
+            var arr = new int[] { 5, 7, 2, 7, 5, 2, 5 };
+            OddNumberofTimes.FindNumber(arr);
+        }
+    }
+}
