ffd68f5d8614ea3cd64da75ecfe65ef82bb02c76
Bianca <biancalbarbosa@hotmail.com>
2024-01-12 14:12:29 -0300

    generic problems solution

---
+++ NonSpecific/ConsoleApp/ConsoleApp/ArrayOperations.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ArrayOperations
+    {
+        public static void SortAsc(int[] arr)
+        {
+            Console.WriteLine(string.Join(' ', arr));
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int y = i+1; y < arr.Length; y++)
+                {
+                    if(arr[i] > arr[y])
+                    {
+                        var temp = arr[i];
+                        arr[i] = arr[y];
+                        arr[y] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        public static void SortDesc(int[] arr)
+        {
+            Console.WriteLine(string.Join(' ', arr));
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int y = i + 1; y < arr.Length; y++)
+                {
+                    if (arr[i] < arr[y])
+                    {
+                        var temp = arr[i];
+                        arr[i] = arr[y];
+                        arr[y] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(' ', arr));
+        }
+
+        public static void FindMin(int[] arr)
+        {
+            int min = arr[0];
+            foreach (var num in arr.Skip(1))
+            {
+                if (num < min) min = num;
+            }
+            Console.WriteLine("Min= " + min);
+        }        
+        public static void FindMax(int[] arr)
+        {
+            int max = arr[0];
+            foreach (var num in arr.Skip(1))
+            {
+                if (num > max) max = num;
+            }
+            Console.WriteLine("Max= " + max);
+        }
+    }
+}
---
+++ NonSpecific/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 2, 9, 4, 3, 5, 1, 7 };
+
+            Console.WriteLine("Array Operations");
+            
+            Console.WriteLine("\nAscending");
+            ArrayOperations.SortAsc(arr);            
+            
+            Console.WriteLine("\nDescending");
+            ArrayOperations.SortDesc(arr);
+
+            Console.WriteLine("\nMininum value");
+            ArrayOperations.FindMin(arr);
+
+            Console.WriteLine("\nMaximum value");
+            ArrayOperations.FindMax(arr);
+        }
+    }
+}
