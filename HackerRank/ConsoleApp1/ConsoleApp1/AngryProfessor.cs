26b8d26041b47229d92f269131f684dde370ffdd
Bianca <biancalbarbosa@hotmail.com>
2024-01-23 09:24:28 -0300

    Add hacker rank solution - AngryProfessor

---
+++ HackerRank/ConsoleApp1/ConsoleApp1/AngryProfessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class AngryProfessor
+    {
+        public static void IsClassCancelled(int k, List<int> a)
+        {
+            var studentsOnTime = a.Where(a => a <= 0).Count();
+            Console.WriteLine("studentsOnTime= " + studentsOnTime);
+            var isClassCancelled = studentsOnTime >= k ? "NO" : "YES";
+            Console.WriteLine(isClassCancelled);
+        }
+    }
+}
