0f4bba34d848bf5d839aae9dfa884f6452ca0fb8
Bianca <biancalbarbosa@hotmail.com>
2024-01-25 12:59:14 -0300

    Add hacker rank solution - BeautifulDaystheMovies

---
+++ HackerRank/ConsoleApp1/ConsoleApp1/BeautifulDaysMovies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class BeautifulDaysMovies
+    {
+        public static void CountBeautifulDays(int i, int j, int k)
+        {
+            int count = 0;
+            var days = new List<int>();
+
+            for (int d = i; d <= j; d++) days.Add(d);
+
+            foreach (var num in days)
+            {
+                var diff = Math.Abs(num - ReverseNum(num));
+                var isEvenlyDiv = diff % k == 0;
+                if (isEvenlyDiv) count++;
+            }
+
+            Console.WriteLine("Beautiful days= " + count);
+        }
+
+        private static int ReverseNum(int num)
+        {
+            var reverse = num.ToString().Reverse().ToArray();
+            return int.Parse(new string(reverse));
+        }
+    }
+}
