af5e03a2c9b1ce9d1dbe8d52e9050c62524771cb
Bianca <biancalbarbosa@hotmail.com>
2024-02-02 15:23:35 -0300

    Add hacker rank solution - EqualizeTheArray

---
+++ HackerRank/ConsoleApp1/ConsoleApp1/EqualizeTheArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class EqualizeTheArray
+    {
+        public static void Calculate(List<int> arr)
+        {
+            var arrGrouped = arr.GroupBy(a => a);
+
+            var maxOcurr = arrGrouped.Select(c => c.Count()).Max();
+
+            Console.WriteLine(arr.Count - maxOcurr);
+        }
+    }
+}
