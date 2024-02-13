97220e7ac375130d49cb44d286c0737bab3047ef
Bianca <biancalbarbosa@hotmail.com>
2024-02-13 12:24:28 -0300

    Add hacker rank solution - BeautifulTriplets

---
+++ HackerRank/ConsoleApp1/ConsoleApp1/BeautifulTriplets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class BeautifulTriplets
+    {
+        public static void CountIsBeautiful(int d, List<int> arr)
+        {
+            var beautiful = new List<List<int>>();
+            for (int i = 0; i < arr.Count; i++)
+            {
+                var current = arr[i];
+                var indexes = new List<int> { i };
+
+                for (int n = i + 1; n < arr.Count; n++)
+                {
+                    var diff = Math.Abs(current - arr[n]);
+                    if (diff == d)
+                    {
+                        indexes.Add(n);
+                        current = arr[n];
+                    }
+                    if (indexes.Count == 3) break;
+                }
+                if (indexes.Count == 3) beautiful.Add(indexes);
+            }
+
+            Console.WriteLine("total= " + beautiful.Count);
+            Console.WriteLine("indexes= ");
+            foreach (var item in beautiful)
+            {
+                Console.WriteLine(string.Join(',', item));
+            }            
+        }
+    }
+}
